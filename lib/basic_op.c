4b1b9564368f5962c4d3fe0a7390acfac54e8599
Kuan Yao <kuanyao@amazon.com>
2017-12-10 20:22:12 -0500

    add motor settings.

--- lib/basic_op.c
+++ lib/basic_op.c
@@ -7,44 +7,44 @@ void runAutonomous(bool withSlewControl);
 bool _autonStarted = false;
 
 void signalAutonCompleted() {
-	writeDebugStreamLine("signal auton off");
-	_autonStarted = false;
+    writeDebugStreamLine("signal auton off");
+    _autonStarted = false;
 }
 
 void checkAutonomous() {
-	if (vexRT[Btn6D] == 1 && vexRT[Btn6U] == 1) {
-		writeDebugStreamLine("try to trigger auton");
-		if (!_autonStarted) {
-			writeDebugStreamLine("starting auton");
-			startTask(triggerAutonomous);
-			//runAutonomous(false);
-			_autonStarted = true;
-		}
-	} else if (vexRT[Btn6D] == 1 && vexRT[Btn6U] == 0) {
-		writeDebugStreamLine("auton off");
-		stopTask(triggerAutonomous);
-		_autonStarted = false;
-	}
+    if (vexRT[Btn6D] == 1 && vexRT[Btn6U] == 1) {
+        writeDebugStreamLine("try to trigger auton");
+        if (!_autonStarted) {
+            writeDebugStreamLine("starting auton");
+            startTask(triggerAutonomous);
+            //runAutonomous(false);
+            _autonStarted = true;
+        }
+    } else if (vexRT[Btn6D] == 1 && vexRT[Btn6U] == 0) {
+        writeDebugStreamLine("auton off");
+        stopTask(triggerAutonomous);
+        _autonStarted = false;
+    }
 }
 
 void sendToWheelMotor(int leftSpeed, int rightSpeed) {
-	// motorReq[LeftFrontMotor]  = speedVeritical + speedHorizontal + speedRotation;
-	// motorReq[RightBackMotor]  = -1 * (speedVeritical + speedHorizontal) + speedRotation;
-	// motorReq[RightFrontMotor] = speedHorizontal - speedVeritical + speedRotation;
-	// motorReq[LeftBackMotor]   = speedVeritical - speedHorizontal + speedRotation;
+    motor[LF] = leftSpeed;
+    motor[LB] = leftSpeed;
+    motor[RF] = rightSpeed;
+    motor[RB] = rightSpeed;
 }
 
 void sendToLiftMotor(int speed) {
-	// motorReq[ClawLeft] = speed;
-	// motorReq[ClawRight] = speed;
+    // motorReq[ClawLeft] = speed;
+    // motorReq[ClawRight] = speed;
 }
 
 void stopBotMovement() {
-	sendToWheelMotor(0, 0);
+    sendToWheelMotor(0, 0);
 }
 
 void rotateBot(int speed) {
-	sendToWheelMotor(speed, -1 * speed);
+    sendToWheelMotor(speed, -1 * speed);
 }
 
 void sendToLiftMotor(int speed) {
